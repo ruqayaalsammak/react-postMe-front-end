c453a6cd67aa7ff079f973e34496cbc00dcc7384
ruqayaalsammak <ruqayaalsammak2017@gmail.com>
2026-08-10 20:52:59 +0300

    Continue working on PostDetails

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,14 +1,15 @@
 // src/pages/PostDetails.jsx
 
-import { useParams } from "react-router"
+import { useNavigate, useParams } from "react-router"
 import * as postService from '../services/posts'
 import { useState, useEffect } from "react"
 
-const PostDetails = () => {
-    const { postId } = useParams()
+const PostDetails = (props) => {
+    const { postId } = useParams();
+    const navigate = useNavigate();
     const [post, setPost] = useState({});
 
-    console.log('postId: ', postId)
+    // console.log('postId: ', postId)
 useEffect(() => {
     const fetchPost = async () => {
         const postData = await postService.show(postId)
@@ -23,7 +24,40 @@ if (!post) return <main>Loading...</main>
 
 
     return (
-    <h1>Post Details</h1>
+    <article className="card post-card">
+        <header className="post-header">
+           <span className="post-category">{post.category?.toUpperCase()}</span> 
+           <h2>{post.title}</h2>
+           <p className="post-author">
+            Posted by {post.author?.username || 'Unknown user'} on <span>{new Date(post.createdAt).toLocaleDateString()}</span>
+           </p>
+           {post.author?._id === props.user?._id && (
+            <div className="actions">
+            <button onClick={() => navigate(`/posts/${postId}/edit`)}>Edit</button>
+            <button onClick={() => props.handleDeletePost(postId)}>Delete</button>
+            </div>
+           )}
+        </header>
+        <p className="post-text">{post.text}</p>
+        <footer className="post-footer">
+        {/* All updates are in the comments section! */}
+        <section>
+        <h2>Comments</h2>
+
+        {!post.comments.length && <p>There are no comments.</p>}
+
+        {post.comments.map((comment) => (
+          <article key={comment._id}>
+            <header>
+              <p>{`${comment.author.username} posted on ${new Date(comment.createdAt).toLocaleDateString()}`}</p>
+            </header>
+            <p>{comment.text}</p>
+          </article>
+        ))}
+      </section>
+
+        </footer>
+    </article>
     
 
     )
